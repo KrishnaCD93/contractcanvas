99c0cb131576478a3a191bfa6b1515c1f7b607ca
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2023-05-15 18:18:19 +0000

    try fix gtag analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,51 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { AppProps } from 'next/app'
 import { Navbar } from '@/components/Navbar';
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 function MyApp({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
-
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+ 
+    router.events.on("routeChangeComplete", handleRouteChange);
+ 
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
-    <ChakraProvider>
-      <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-        <Navbar />
-        <Component {...pageProps} />
-      </SessionContextProvider>
-    </ChakraProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TKK9MLSFV1"></Script>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TKK9MLSFV1', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <ChakraProvider>
+        <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+          <Navbar />
+          <Component {...pageProps} />
+        </SessionContextProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,23 +4,7 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <div className="container">
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-TKK9MLSFV1');
-        `}
-      </Script>
-    </div>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
