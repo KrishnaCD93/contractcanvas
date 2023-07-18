e85e5f9c39e680176d16185c1e739f0425a0d4f4
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2023-07-17 23:13:36 -0500

    streaming

--- src/pages/contract.tsx
+++ src/pages/contract.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Heading,
@@ -9,33 +9,33 @@ import {
 
 const ContractPage = () => {
   const [input, setInput] = useState('');
-  const [output, setOutput] = useState({
-    prediction: {
-      content: '',
-    },
-    error: '',
-  })
+  const [output, setOutput] = useState('');
   const [messages, setMessages] = useState([{ humanMessage: '', aiMessage: '' }])
   const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    const server = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BACKEND_URL : 'http://127.0.0.1:8000';
+    const inputs = JSON.stringify(messages);
+    const eventSource = new EventSource(`${server}/predict/${inputs}`);
+    eventSource.onmessage = (event) => {
+      setOutput(event.data);
+      setMessages([...messages, { humanMessage: input, aiMessage: event.data }])
+    };
+    return () => {
+      eventSource.close();
+    };
+  }, [input, messages]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     try {
-      const server = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BACKEND_URL : 'http://127.0.0.1:8000';
       const newInputs = [...messages, { humanMessage: input, aiMessage: '' }];
-      const inputs = JSON.stringify(newInputs);
-      await fetch(`${server}/predict/${inputs}`)
-        .then((res) => res.json())
-        .then((data) => { 
-          console.log('data: ', data); 
-          setOutput(data);
-          setMessages([...messages, { humanMessage: input, aiMessage: data.prediction }])
-        });
+      setMessages(newInputs);
     } catch (err) {
       alert(err);
     } finally {
@@ -54,10 +54,9 @@ const ContractPage = () => {
           <Button isLoading={loading} type="submit">Get Response</Button>
         </form>
         {
-          output.prediction && (
+          output && (
             <Box>
-              <p>{output.prediction.content}</p>
-              <p color='red'>{output.error}</p>
+              <p>{output}</p>
             </Box>
           )
         }
